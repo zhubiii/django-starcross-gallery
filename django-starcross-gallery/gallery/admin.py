5ee5e5c99234e7c55a179d35ae0dfbb5e0e62454
Brian Zhu <fromthemuses.db@gmail.com>
2020-08-12 19:47:25 +0000

    Added validated/incomplete info filters. Also mass mark validated action in admin

--- django-starcross-gallery/gallery/admin.py
+++ django-starcross-gallery/gallery/admin.py
@@ -10,13 +10,19 @@ else:
     class SortableAdminMixin:
         mock = True
 
+def make_validated(modeladmin, request, queryset):
+
+    queryset.update(is_validated=True)
+
+make_validated.short_description = "Mark selected photos as validated"
 
 class ImageAdmin(admin.ModelAdmin):
     admin_thumbnail = AdminThumbnail(image_field='data_thumbnail', template='gallery/admin/thumbnail.html')
     list_display = ('title', 'admin_thumbnail', 'date_taken', 'date_uploaded')
-    list_filter = ('image_albums',)
+    list_filter = ('image_albums', 'is_validated','incomplete_info' )
     list_per_page = 25
     readonly_fields = ('admin_thumbnail',)
+    actions = [make_validated]
 
 
 class AlbumAdmin(SortableAdminMixin, admin.ModelAdmin):
